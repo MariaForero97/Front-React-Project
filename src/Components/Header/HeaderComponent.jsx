8adbfb3e92b2f87b4fa4ffbb10c73a547624458a
MariaForero97 <mariaforero20210@gmail.com>
2022-08-23 09:24:36 -0500

    Avance proyecto Login solarise

--- src/Components/Header/HeaderComponent.jsx
+++ src/Components/Header/HeaderComponent.jsx
@@ -1,8 +1,18 @@
 import React, { useState } from 'react';
-function HeaderComponent() {
+//import video1 from '../../Files/video1.mp4';
+//import video2 from '../../Files/video2.mp4';
+//import video3 from '../../Files/video3.mp4';
+//import video4 from '../../Files/video4.mp4';
+function HeaderComponent(props) {
 
     const [video, setVideo] = useState(0);
 
+    /*const urls = [
+        video1,
+        video2,
+        video3,
+        video4,
+    ];*/
 
     const handleEnded = (e) => {
         setVideo(Math.round(Math.random() * (3 - 0)));
@@ -11,12 +21,16 @@ function HeaderComponent() {
     return (
         <React.Fragment>
             <header className="masthead viewport-header">
+                <video id="background-video bgvid" loop muted autoPlay onEnded={handleEnded}>
+                   
+                </video>
+
                 <div className="container position-relative px-4 px-lg-5">
                     <div className="row gx-4 gx-lg-5 justify-content-center">
                         <div className="col-md-10 col-lg-8 col-xl-7">
                             <div className="page-heading">
-                                <h1>Bienvenidos</h1>
-                                <span className="subheading" id="title-page">Conectado como: Yo</span>
+                                <h1>{(props.USER_CONNECTED && Object.entries(props.USER_CONNECTED).length > 0) ? "Bienvenidos" : ""}</h1>
+                                <span className="subheading" id="title-page">{(props.USER_CONNECTED && Object.entries(props.USER_CONNECTED).length > 0) ? "Conectado como: " + props.USER_CONNECTED.name : ""}</span>
                             </div>
                         </div>
                     </div>
--- src/Components/Login/LoginComponent.jsx
+++ src/Components/Login/LoginComponent.jsx
@@ -6,6 +6,8 @@ import { AppContext } from "../../Providers/AppProvider";
 import { getUserMain } from "../../Services/UserService";
 import { USER_CONNECTED } from "../../Util/Constants";
 import { UseLocalStorage } from "../../Util/UseLocalStorage";
+import SolaRiseLogosTransparent from '../../Files/SolaRise-logos_transparent.png';
+import Panel from '../../Files/Panel.jpg';
 
 export default function LoginComponent() {
 
@@ -55,41 +57,78 @@ export default function LoginComponent() {
     }
 
     return (
-        <div className="Login">
-            <div className="post-preview" style={{ textAlign: "center", marginTop: "-30px" }}>
-                <h2 className="post-title">Bienvenidos a Smile</h2>
-                <p className="post-meta">
-                    “Puedes conseguir lo que quieras si vistes para ello”
-                </p>
-            </div>
+        <Form.Group class="container-main">
+        <Form.Group className="container-login">
+            <div className="Login">
+                <div className="post-preview" style={{ textAlign: "center", marginTop: "40px" }}>
+                    <h2 className="post-title">Login</h2>
+                </div>
             <br />
             <Form onSubmit={handleSubmit}>
                 <div className="form-floating">
+                
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                
                     <input className="form-control" id="name" type="text"
                         value={userLogin}
                         onChange={(e) => setUserLogin(e.target.value)}
-                        placeholder="Ingresa tu nombre..." />
-                    <label htmlFor="name">Usuario</label>
+                        />
+                </Form.Group>
                 </div>
                 <div className="form-floating">
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                
                     <input className="form-control" id="password" type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Ingresa tu contraseña..." />
-                    <label htmlFor="password">Contraseña</label>
-                </div>
+                        />
+                </Form.Group>
                 <br />
                 <div style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center"
+                    display:"flex",
+                    justifyContent: "right",
+                    alignItems: "center",
+                    marginInline: -1,
+                    marginTop: -20,
                 }}>
-                    <Button className="btn btn-primary text-uppercase" block size="lg" type="submit" disabled={!validateForm()}>
-                        Ingresar
+                    <Form.Group className="btn">
+                    <Button style={{
+                        backgroundColor: "#4B8D97",
+                        borderColor:"#4B8D97",
+                        alignItems:"center"
+
+                    }} 
+                    className="btn btn-primary" 
+                    variant="lg"
+                    type="submit" 
+                    disabled={!validateForm()}>
+                        Login
                     </Button>
+                    </Form.Group>
                 </div>
-            </Form >
-        </div >
+                </div>
+                
+                </Form >
+            </div >
+    </Form.Group>
+    <div className="Container-imgPrincipal">
+        <div className="container-img2">
+        <img
+            className="img-PanelPrincipal"
+            src={Panel}
+        />
+    </div>
+    <div className="container-img">
+        <img
+            className="img-Solarise-logo"
+            src={SolaRiseLogosTransparent}
+        />
+    </div>
+    
+    </div>
+    </Form.Group>
     )
 }
 
